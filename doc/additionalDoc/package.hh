dcc8a4c38889f5a5e3b8a5bb3d9da42ec45495b1
Francois Bleibel <fbleibel@gmail.com>
2010-09-09 17:37:59 +0900

    Added uninstall target. Started documentation.

--- doc/additionalDoc/package.hh
+++ doc/additionalDoc/package.hh
@@ -3,6 +3,13 @@
 
 \section intro_dg-middleware Introduction
 
-This package implements ...
+The dynamic-graph middleware package contains all 'middleware' components for the stack of tasks v2 operation.
+This includes:
+- The ServerCommand entity (load server-abstract.so and server-command.so)
+- The DistantShell entity (distant-shell.so)
+-
+
+
+\section How to use this package
 
 */
