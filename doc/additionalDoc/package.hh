84b6ac6a51674a9225584ac2c0a99ab705f9c2d3
Francois Bleibel <fbleibel@gmail.com>
2010-09-10 16:37:45 +0900

    Added (succint) documentation for dg-middleware.

--- doc/additionalDoc/package.hh
+++ doc/additionalDoc/package.hh
@@ -5,11 +5,18 @@
 
 The dynamic-graph middleware package contains all 'middleware' components for the stack of tasks v2 operation.
 This includes:
-- The ServerCommand entity (load server-abstract.so and server-command.so)
-- The DistantShell entity (distant-shell.so)
--
+- The ServerCommand entity (load both server-abstract.so and server-command.so)
+  This entity allows Corba connections into it, and executions of commands through
+  the commandLine (see server-command.h for the list of supported functions)
+- The DistantShell entity (distant-shell.so) is a server-side entity that executes
+  files remotely.
+- The coshell program (installs in bin/sot) can be used to connect to the server remotely.
 
 
 \section How to use this package
+If you wish to expose a corba server to your potential clients, simply load and create the
+ServerCommand entity in your library.
+
+Programs who wish to connect to your server can use the coshell executable.
 
 */
